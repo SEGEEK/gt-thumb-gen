f3db9bbf43bc76b1abdb8f11900f28fec1c5c8f2
SEGEEK <davidt.hudspeth@gmail.com>
2019-05-29 20:50:15 -0500

    Cleanup image uri converter
    Skip null check in favor of strict null analysis.
    Reject promise on error.

--- ImageUtil.ts
+++ ImageUtil.ts
@@ -1,13 +1,14 @@
 export async function convertURIToImageData(URI: string) {
     return new Promise<HTMLImageElement>(function (resolve, reject) {
-        if (URI == null) {
-            return reject();
-        }
         var image = new Image();
         image.addEventListener('load', function () {
             resolve(image);
-        }, false);
-        //TODO reject on 'error' event?
+        });
+        
+        image.addEventListener('error', function(ev:ErrorEvent){
+            reject(ev.error);
+        });
+
         image.setAttribute('src', URI);
     });
 }
